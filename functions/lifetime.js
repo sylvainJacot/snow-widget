8a762f2e3120972d8cf1e7c2965fb8238c7ecd3c
Sylvain <jct.sylvain@gmail.com>
2025-04-29 22:19:53 +0200

    test life time

--- functions/lifetime.js
+++ functions/lifetime.js
@@ -1,7 +1,7 @@
 export function isWithinLifetime(config) {
     const now = new Date();
-    const start = config.startDate ? new Date(config.startDate) : null;
-    const end = config.endDate ? new Date(config.endDate) : null;
+    const start = config.startDate ? new Date(config.startDate.replace('T', ' ') + ':00') : null;
+    const end = config.endDate ? new Date(config.endDate.replace('T', ' ') + ':00') : null;
 
     if (start && end) {
         return now >= start && now <= end;
